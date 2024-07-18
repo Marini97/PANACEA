1407f2a48f587ed94ce44068e2bcf7f59199e69a
Tommaso Caiazzi <tommasocaiazzi@gmail.com>
2024-07-18 16:55:41 +0200

    Fix plots

--- plot.py
+++ plot.py
@@ -207,106 +207,165 @@ def plot_reward_figure(result_path):
         os.path.join(experiment3_figures_path, f"reward_figure.pdf"), format="pdf", bbox_inches='tight'
     )
 
+def parse_dot_file(file_path):
+    results = []
+    with open(file_path, "r") as dot_file:
+        lines = dot_file.readlines()
+    for line in lines:
+        if ":" not in line:
+            continue
+        results.append(line.split(":")[-1].split("\"")[0])
+    return results
 
-def plot_reward_single_multi(prism_path, result_path, figures_path):
+def parse_csv_file(file_path):
+    results = []
+    with open(file_path, "r") as csv_file:
+        lines = csv_file.readlines()
+    for line in lines:
+        # [action]
+        if "[" not in line:
+            continue
+        results.append(line.split("[")[1].split("]")[0])
+    return results
+
+def parse_defender_file(file_path):
+    # get last element of each row if it's an integer
+    parsed_result = [int(row[-1]) for row in csv.reader(open(file_path), delimiter=",") if
+                     row and row[-1].isdigit()]
+    reward = sum(parsed_result) / len(parsed_result)
+    return reward
+
+def get_actions_rewards(prism_path, agent):
+    results = {}
+    reward_pattern = re.compile(rf'rewards "{agent}"(.*?)endrewards', re.DOTALL)
+    with open(prism_path, "r") as prism_file:
+        lines = prism_file.read()
+    match = reward_pattern.search(lines)
+
+    actions_rewards = match.group(1).strip().split(";")
+    for line in actions_rewards:
+        match = re.search(r'\[(.*?)\].*:\s*(\d+)', line)
+        if match:
+            action = match.group(1)
+            cost = match.group(2)
+            results[action] = int(cost)
+    return results
+
+def compute_reward(actions, actions_rewards, attacker_goal=None):
+    reward = 0
+    att_winner = False
+    for action in actions:
+        if action in actions_rewards:
+            if attacker_goal and action == attacker_goal:
+                att_winner = True
+                continue
+            reward += int(actions_rewards[action])
+    return reward, att_winner
+
+def compute_rewards(experiment_name, result_path, prism_path):
+    experiment_path = os.path.join(result_path, experiment_name)
+    if "defender" in experiment_name:
+        defender_reward = parse_defender_file(os.path.join(experiment_path, f"{experiment_name}.csv"))
+        return 0, defender_reward
+    if os.path.exists(os.path.join(experiment_path, f"{experiment_name}.dot")):
+        dot_path = os.path.join(experiment_path, f"{experiment_name}.dot")
+        actions = parse_dot_file(dot_path)
+    elif os.path.exists(os.path.join(experiment_path, f"{experiment_name}.csv")):
+        csv_file = os.path.join(experiment_path, f"{experiment_name}.csv")
+        actions = parse_csv_file(csv_file)
+
+    prism_path = os.path.join(prism_path, f"{experiment_name}.prism")
+    attacker_rewards = get_actions_rewards(prism_path, "attacker")
+    defender_rewards = get_actions_rewards(prism_path, "defender")
+    attacker_reward, _ = compute_reward(actions, attacker_rewards)
+    defender_reward, attacker_winner = compute_reward(actions, defender_rewards, "exfiltrateData")
+    return attacker_reward, defender_reward, attacker_winner
+
+def plot_reward_adt_mdp(prism_path, result_path, figures_path, experiments_order = None):
     """
     histogram with rewards from experiment
     """
-    def parse_dot_file(file_path):
-        results = []
-        with open(file_path, "r") as dot_file:
-            lines = dot_file.readlines()
-        for line in lines:
-            if ":" not in line:
-                continue
-            results.append(line.split(":")[-1].split("\"")[0])
-        return results
-    
-    def parse_csv_file(file_path):
-        results = []
-        with open(file_path, "r") as csv_file:
-            lines = csv_file.readlines()
-        for line in lines:
-            # [action]
-            if "[" not in line:
-                continue
-            results.append(line.split("[")[1].split("]")[0])
-        return results
-    
-    def parse_defender_file(file_path):
-        # get last element of each row if it's an integer
-        parsed_result = [ int(row[-1]) for row in csv.reader(open(file_path), delimiter=",") if row and row[-1].isdigit()]
-        reward = sum(parsed_result) / len(parsed_result)
-        return reward
-
-    def get_actions_rewards(prism_path, agent):
-        results = {}
-        reward_pattern = re.compile(rf'rewards "{agent}"(.*?)endrewards', re.DOTALL)
-        with open(prism_path, "r") as prism_file:
-            lines = prism_file.read()
-        match = reward_pattern.search(lines)
-
-        actions_rewards = match.group(1).strip().split(";")
-        for line in actions_rewards:
-            match = re.search(r'\[(.*?)\].*:\s*(\d+)', line)
-            if match:
-                action = match.group(1)
-                cost = match.group(2)
-                results[action] = int(cost)
-        return results
-
-    def compute_reward(actions, actions_rewards):
-        reward = 0
-        for action in actions:
-            if action in actions_rewards:
-                reward += int(actions_rewards[action])
-        return reward
-
-    def compute_rewards(experiment_name, prism_path):
-        experiment_path = os.path.join(result_path, experiment_name)
-        if "efender" in experiment_name:
-            defender_reward = parse_defender_file(os.path.join(experiment_path, f"{experiment_name}.csv"))
-            return 0, defender_reward
-        if os.path.exists(os.path.join(experiment_path, f"{experiment_name}.dot")):
-            dot_path = os.path.join(experiment_path, f"{experiment_name}.dot")
-            actions = parse_dot_file(dot_path)
-        elif os.path.exists(os.path.join(experiment_path, f"{experiment_name}.csv")):
-            csv_file = os.path.join(experiment_path, f"{experiment_name}.csv")
-            actions = parse_csv_file(csv_file)
-            
-        prism_path = os.path.join(prism_path, f"{experiment_name}.prism")
-        attacker_rewards = get_actions_rewards(prism_path, "attacker")
-        defender_rewards = get_actions_rewards(prism_path, "defender")
-        attacker_reward = compute_reward(actions, attacker_rewards)
-        defender_reward = compute_reward(actions, defender_rewards)
-        return attacker_reward, defender_reward
-    
     plt.clf()
     plt.grid(linestyle='--', linewidth=0.5)
 
     to_plot = {'x': []}
     rewards = {'Att': [], 'Def': []}
-    offset = 5
-    
-    for experiment_name in sorted(os.listdir(result_path)):
-        
-        ar, dr = compute_rewards(experiment_name, prism_path)
+
+    x_values = []
+    offset = 0
+    for experiment_name in sorted(os.listdir(result_path)) if not experiments_order else experiments_order:
+        ar, dr, att_win = compute_rewards(experiment_name, result_path, prism_path)
         to_plot['x'].append(experiment_name.replace("_", " "))
         rewards['Att'].append(ar)
         rewards['Def'].append(dr)
-        offset += 5
-        
-    x_values = [x for x in range(0, len(to_plot['x'])*10, 10)]
-    
-    plt.bar(x_values, np.array(rewards['Att']), label="Att", fill=None,
+
+        x_values.append(offset)
+        if att_win:
+            plt.text(offset - 0.5, ar + 10, "W")
+            plt.text(offset + 3.5, dr + 10, "L")
+        else:
+            plt.text(offset - 0.5, ar + 10, "L")
+            plt.text(offset + 3.5, dr + 10, "W")
+
+        offset += 10
+
+    plt.bar(x_values, np.array(rewards['Att']), label="Attacker", fill=None,
             hatch="////", edgecolor="blue", width=4)
-    plt.bar(x_values, np.array(rewards['Def']), label="Def",
-            bottom=np.array(rewards['Att']), fill=None, hatch="////", edgecolor="green", width=4)
+    plt.bar(list(map(lambda x: x[1] + 4 if rewards["Att"][x[0]] and rewards["Def"][x[0]] else x[1], enumerate(x_values))), np.array(rewards['Def']), label="Defender", fill=None, hatch="////", edgecolor="green", width=4)
     
-    plt.xticks(x_values, to_plot['x'], fontsize=7)
-    #plt.yticks(range(0, 7000, 1000))
+    plt.xticks(list(map(lambda x: x[1] + 2 if rewards["Att"][x[0]] and rewards["Def"][x[0]] else x[1], enumerate(x_values))), to_plot['x'], fontsize=7)
+    plt.yticks(range(0, 800, 200))
     
     #plt.xlabel('Experiment')
+    plt.ylabel('Cost')
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.2), labelspacing=0.2, ncols=2, prop={'size': 6})
+
+    plt.savefig(
+        os.path.join(figures_path, f"reward_solo_multi_figure.pdf"), format="pdf", bbox_inches='tight'
+    )
+
+
+def plot_reward_single_multi(prism_path, result_path, figures_path, experiments_order=None):
+    """
+    histogram with rewards from experiment
+    """
+    plt.clf()
+    plt.grid(linestyle='--', linewidth=0.5)
+
+    to_plot = {'x': []}
+    rewards = {'Att': [], 'Def': []}
+
+    x_values = []
+    offset = 0
+    for experiment_name in sorted(os.listdir(result_path)) if not experiments_order else experiments_order:
+        ar, dr, att_win = compute_rewards(experiment_name, result_path, prism_path)
+        to_plot['x'].append(experiment_name.replace("_", " "))
+        rewards['Att'].append(ar)
+        rewards['Def'].append(dr)
+        print(experiment_name, ar, dr)
+        x_values.append(offset)
+        if ar > 0 and dr > 0:
+            if att_win:
+                plt.text(offset - 0.5, ar + 10, "W")
+                plt.text(offset + 3.5, dr + 10, "L")
+            else:
+                plt.text(offset - 0.5, ar + 10, "L")
+                plt.text(offset + 3.5, dr + 10, "W")
+
+        offset += 10
+
+    plt.bar(x_values, np.array(rewards['Att']), label="Attacker", fill=None,
+            hatch="////", edgecolor="blue", width=4)
+    plt.bar(
+        list(map(lambda x: x[1] + 4 if rewards["Att"][x[0]] and rewards["Def"][x[0]] else x[1], enumerate(x_values))),
+        np.array(rewards['Def']), label="Defender", fill=None, hatch="////", edgecolor="green", width=4)
+
+    plt.xticks(
+        list(map(lambda x: x[1] + 2 if rewards["Att"][x[0]] and rewards["Def"][x[0]] else x[1], enumerate(x_values))),
+        to_plot['x'], fontsize=7)
+    plt.yticks(range(0, 800, 200))
+
+    # plt.xlabel('Experiment')
     plt.ylabel('Cost')
     plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.2), labelspacing=0.2, ncols=2, prop={'size': 6})
 
@@ -323,7 +382,7 @@ if __name__ == '__main__':
     experiment1_prism_path = os.path.join(experiment1_path, "prism")
     experiment1_results_path = os.path.join(experiment1_path, "results")
     experiment1_figures_path = os.path.join(experiment1_path, "figures")
-    
+
     experiment2_path = os.path.join("experiments", "experiment2")
     experiment2_prism_path = os.path.join(experiment2_path, "prism")
     experiment2_results_path = os.path.join(experiment2_path, "results")
@@ -334,13 +393,21 @@ if __name__ == '__main__':
     experiment3_results_path = os.path.join(experiment3_path, "results")
     experiment3_figures_path = os.path.join(experiment3_path, "figures")
 
+    experiment4_path = os.path.join("experiments", "experiment4")
+    experiment4_prism_path = os.path.join(experiment4_path, "prism")
+    experiment4_results_path = os.path.join(experiment4_path, "results")
+    experiment4_figures_path = os.path.join(experiment4_path, "figures")
+
     
     os.makedirs(experiment1_figures_path, exist_ok=True)
     os.makedirs(experiment2_figures_path, exist_ok=True)
     os.makedirs(experiment3_figures_path, exist_ok=True)
     plot_reward_figure(experiment3_results_path)
-    plot_reward_single_multi(experiment1_prism_path, experiment1_results_path, experiment1_figures_path)
-    plot_reward_single_multi(experiment2_prism_path, experiment2_results_path, experiment2_figures_path)
+    plot_reward_adt_mdp(experiment1_prism_path, experiment1_results_path, experiment1_figures_path,
+                        ["R-ADT_LC", "R-ADT_LD", "PANACEA"])
+    plot_reward_adt_mdp(experiment2_prism_path, experiment2_results_path, experiment2_figures_path,
+                        ["R-ADT_LC", "R-ADT_LD", "PANACEA"])
+    plot_reward_single_multi(experiment4_prism_path, experiment4_results_path, experiment4_figures_path)
     plot_time_size_figure(os.path.join(experiment3_path, "result.csv"))
     plot_mdp_size_tree_size_figure(os.path.join(experiment3_path, "result.csv"))
     plot_mdp_size_time_figure(os.path.join(experiment3_path, "result.csv"))
---
+++ run_experiment4.sh
@@ -0,0 +1,48 @@
+DIRECTORY=experiments/experiment4
+PRISM_PATH=../prism-games-3.2.1-linux64-x86
+JVM_MEMORY=$1
+CUDD_MEMORY=$2
+
+rm -rf "$DIRECTORY"/prism/
+rm -rf "$DIRECTORY"/results/
+mkdir "$DIRECTORY"/prism/
+mkdir "$DIRECTORY"/results/
+
+for TREE_FILE in "$DIRECTORY"/trees/*.xml; do
+    BASENAME=$(basename "$TREE_FILE" .xml)
+    
+    python3 exp3.py -i "$TREE_FILE"
+done
+
+# Loop over each .prism file in the directory
+for MODEL_FILE in "$DIRECTORY"/prism/*.prism; do
+  # Extract the base name of the file (without the directory and extension)
+  BASENAME=$(basename "$MODEL_FILE" .prism)
+  EXPERIMENT_DIRECTORY="$DIRECTORY"/results/"$BASENAME"
+  mkdir -p $EXPERIMENT_DIRECTORY
+
+  # if basename contains "defender" then it does the experiment with all possible values
+  if [[ $BASENAME == *"efender"* ]]; then
+    # get constants needed to run the experiment
+    CONSTANTS=$(grep "const" "$MODEL_FILE" | awk '{print $2}')
+    # replace ";" with "=0:2," at the end of each constant
+    CONSTANTS=$(echo $CONSTANTS | sed 's/; /=0:2,/g')
+    CONSTANTS=$(echo $CONSTANTS | sed 's/;/=0:2/g')
+     # Run the command with /usr/bin/time and redirect the output to the respective file
+    /usr/bin/time -v "$PRISM_PATH"/bin/prism -javamaxmem "$JVM_MEMORY"g -cuddmaxmem "$CUDD_MEMORY"g "$MODEL_FILE" "$DIRECTORY"/prism/defender.props -prop 1 -const "$CONSTANTS" -exportresults $EXPERIMENT_DIRECTORY/"$BASENAME".csv:csv  -exportstrat $EXPERIMENT_DIRECTORY/"$BASENAME".dot 2>&1 | tee $EXPERIMENT_DIRECTORY/"$BASENAME".log
+  else
+    # Run the command with /usr/bin/time and redirect the output to the respective file
+    /usr/bin/time -v "$PRISM_PATH"/bin/prism -javamaxmem "$JVM_MEMORY"g -cuddmaxmem "$CUDD_MEMORY"g "$MODEL_FILE" "$DIRECTORY"/prism/properties.props -prop 1 -exportstrat $EXPERIMENT_DIRECTORY/"$BASENAME".dot 2>&1 | tee $EXPERIMENT_DIRECTORY/"$BASENAME".log
+  fi
+
+done
+
+# copy the gamma=0 files inside folders
+for FOLDER in "$DIRECTORY"/*; do
+  if [[ $FOLDER != *"figures"* ]] && [[ $FOLDER != *"prism"* ]] && [[ $FOLDER != *"results"* ]] && [[ $FOLDER != *"trees"* ]]; then
+    cp -r $FOLDER "$DIRECTORY"/results/
+  fi
+  if [[ $FOLDER == *".prism" ]]; then
+    cp -r $FOLDER "$DIRECTORY"/prism/
+  fi
+done
